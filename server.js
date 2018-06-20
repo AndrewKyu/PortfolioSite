aee06e908f320016d5dc26253081c9042e9de708
AndrewKyu <andrewkyu95@gmail.com>
2018-06-20 02:52:40 -0700

    Submit button on contact me now forms an JSON object

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
+const path = require("path");
 const nodemailer = require("nodemailer");
 
 const app = express();
 
-//create route
+// View engine setup
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
+// Static folder
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.render("index");
+});
+
+app.post("/send", (req, res) => {
+  console.log(req.body);
 });
 
 app.listen(3000, () => console.log("Server started"));
