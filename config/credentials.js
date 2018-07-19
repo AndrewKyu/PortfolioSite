670089c2db3d1459a27a0d78f21d8053afe3bb37
Andrew Kyu <andrewkyu95@gmail.com>
2018-07-19 00:46:17 -0700

    removed configs

--- config/credentials.js
+++
@@ -1,5 +0,0 @@
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./credentials_prod");
-} else {
-  module.exports = require("./credentials_dev");
-}
--- config/credentials_prod.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  toEmail: process.env.TO_EMAIL,
-  password: process.env.PASSWORD
-};
