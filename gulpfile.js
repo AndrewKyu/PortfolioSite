5b954badf243227d52276b354cae99dfb2350e96
Andrew Kyu <andrewkyu95@gmail.com>
2018-07-29 15:52:54 -0700

    test

--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,25 @@ const imagemin = require("gulp-imagemin");
 const uglify = require("gulp-uglify");
 const cleanCSS = require("gulp-clean-css");
 
-gulp.task("imageMin", () =>
-  gulp
-    .src("public/img/*")
-    .pipe(imagemin())
-    .pipe(gulp.dest("dist/images"))
-);
+// gulp.task("imageMin", () =>
+//   gulp
+//     .src("public/img/*")
+//     .pipe(imagemin())
+//     .pipe(gulp.dest("dist/images"))
+// );
 
-gulp.task("minify", () =>
-  gulp
-    .src("public/js/*.js")
-    .pipe(uglify())
-    .pipe(gulp.dest("dist/js"))
-);
+// gulp.task("minify", () =>
+//   gulp
+//     .src("public/js/*.js")
+//     .pipe(uglify())
+//     .pipe(gulp.dest("dist/js"))
+// );
 
-gulp.task("minify-css", () => {
-  return gulp
-    .src("public/css/style.css")
-    .pipe(cleanCSS())
-    .pipe(gulp.dest("dist/css"));
-});
+// gulp.task("minify-css", () => {
+//   return gulp
+//     .src("public/css/style.css")
+//     .pipe(cleanCSS())
+//     .pipe(gulp.dest("dist/css"));
+// });
 
-gulp.task("default", ["minify-css"]);
+gulp.task("default", () => {});
