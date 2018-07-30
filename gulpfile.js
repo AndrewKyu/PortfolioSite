c7c80c732f2e017c54067690e3fe877a08f4adfc
Andrew Kyu <andrewkyu95@gmail.com>
2018-07-29 18:33:59 -0700

    compressed all CSS files together using gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,24 @@
 const gulp = require("gulp");
+const nodemon = require("gulp-nodemon");
 const imagemin = require("gulp-imagemin");
 const uglify = require("gulp-uglify");
 const cleanCSS = require("gulp-clean-css");
+const sourcemaps = require("gulp-sourcemaps");
+const rename = require("gulp-rename");
+const concat = require("gulp-concat");
+
+gulp.task("nodemon", cb => {
+  var started = false;
+
+  return nodemon({
+    script: "server.js"
+  }).on("start", () => {
+    if (!started) {
+      cb();
+      started = true;
+    }
+  });
+});
 
 // gulp.task("imageMin", () =>
 //   gulp
@@ -17,11 +34,12 @@ const cleanCSS = require("gulp-clean-css");
 //     .pipe(gulp.dest("dist/js"))
 // );
 
-// gulp.task("minify-css", () => {
-//   return gulp
-//     .src("public/css/style.css")
-//     .pipe(cleanCSS())
-//     .pipe(gulp.dest("dist/css"));
-// });
+gulp.task("minify-css", () => {
+  return gulp
+    .src("./public/css/*.css")
+    .pipe(cleanCSS())
+    .pipe(concat("style.min.css"))
+    .pipe(gulp.dest("./public/css"));
+});
 
-gulp.task("default", () => {});
+gulp.task("default", ["nodemon"]);
