e75c1043030187628a16c1447ece25121a1cdad5
Andrew Kyu <andrewkyu95@gmail.com>
2018-07-29 18:47:48 -0700

    compressed CSS using gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,43 @@
 const gulp = require("gulp");
+const nodemon = require("gulp-nodemon");
 const imagemin = require("gulp-imagemin");
 const uglify = require("gulp-uglify");
 const cleanCSS = require("gulp-clean-css");
+const concat = require("gulp-concat");
 
-gulp.task("imageMin", () =>
-  gulp
-    .src("public/img/*")
-    .pipe(imagemin())
-    .pipe(gulp.dest("dist/images"))
-);
+gulp.task("nodemon", cb => {
+  var started = false;
 
-gulp.task("minify", () =>
-  gulp
-    .src("public/js/*.js")
-    .pipe(uglify())
-    .pipe(gulp.dest("dist/js"))
-);
+  return nodemon({
+    script: "server.js"
+  }).on("start", () => {
+    if (!started) {
+      cb();
+      started = true;
+    }
+  });
+});
+
+// gulp.task("imageMin", () =>
+//   gulp
+//     .src("public/img/*")
+//     .pipe(imagemin())
+//     .pipe(gulp.dest("dist/images"))
+// );
+
+// gulp.task("minify", () =>
+//   gulp
+//     .src("public/js/*.js")
+//     .pipe(uglify())
+//     .pipe(gulp.dest("dist/js"))
+// );
 
 gulp.task("minify-css", () => {
   return gulp
-    .src("public/css/style.css")
+    .src("./public/css/*.css")
     .pipe(cleanCSS())
-    .pipe(gulp.dest("dist/css"));
+    .pipe(concat("style.min.css"))
+    .pipe(gulp.dest("./public/css"));
 });
 
-gulp.task("default", ["minify-css"]);
+gulp.task("default", ["nodemon"]);
